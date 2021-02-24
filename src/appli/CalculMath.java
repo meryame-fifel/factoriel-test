5f5b8c4b5ffc7887be8f113e05010d56665dc272
meryame-fifel <63552491+meryame-fifel@users.noreply.github.com>
2021-02-24 23:37:53 +0100

    Update CalculMath.java

--- src/appli/CalculMath.java
+++ src/appli/CalculMath.java
@@ -3,8 +3,8 @@ package appli;
 public class CalculMath {
 
 	public int fac(int val) {
-		int result = 1;
-		for (int k = val; k > 0; k--) {
+		int result = val;
+		for (int k = val - 1; k > 0; k--) {
 			result *=k;
 		}
 		return result;
